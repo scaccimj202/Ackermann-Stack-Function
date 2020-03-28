2429e578bf957136ecca4ba5505de5d168e3265d
Matt Scaccia <scaccimj202@potsdam.edu>
2020-03-28 15:34:14 -0400

    Initial commit

---
+++ Ackermann.java
@@ -0,0 +1,37 @@
+/**
+ *  Calculate the Ackermann function A(M, N) using a straightforward
+ *  recursive program.
+ *
+ * Compilation:  javac Ackermann.java
+ *  Execution:    java Ackermann M N
+ *
+ *  % java Ackermann 3 8
+ *  2045
+ *
+ *  % java Ackermann 3 9
+ *  StackOverflowError
+ *
+ * Updated 06 March 2020 by  L. Grabowski
+ * Downloaded from:
+ *    https://introcs.cs.princeton.edu/java/53universality/Ackermann.java
+ *
+ * @author L. Grabowski
+ * @email grabowlm@potsdam.edu
+ * @course CIS 303 Algorithm Analysis and Design
+ * Assignment 4b
+*/
+import java.util.*;
+
+public class Ackermann {
+    public static void main(String[] args) {
+        long M = Long.parseLong(args[0]);
+        long N = Long.parseLong(args[1]);
+        System.out.println(ackermann(M, N));
+    }
+    
+    public static long ackermann(long m, long n) {
+        if (m == 0) return n + 1;
+        if (n == 0) return ackermann(m - 1, 1);
+        return ackermann(m - 1, ackermann(m, n - 1));
+    }
+}
